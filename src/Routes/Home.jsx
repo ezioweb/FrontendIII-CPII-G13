2c87d4b8b7cd0131b10bf9588d5b36bdf8021478
ezioweb <ezioweb@hotmail.com>
2022-12-08 00:01:00 -0300

    fetch iniciado

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,19 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Card from "../Components/Card";
 import { ctdUrl } from "../urls";
 
 const Home = () => {
+
+  const [dentistaData, setDentistaData] = useState([]);
 
   useEffect(() => {
     //Nesse useEffect, deverá ser obtido todos os dentistas da API
     //Armazena-los em um estado para posteriormente fazer um map
     //Usando o componente <Card />
     fetch(`${ctdUrl}${'dentista'}`).then(
-      response => console.log(response)
-      )
-      
-    
-
+      response => response.json().then((data) => {
+        setDentistaData(data);
+        console.log(data);
+      })
+    );
   }, []);
 
 
@@ -23,7 +25,20 @@ const Home = () => {
       <h1>Home</h1>
       <div className="card-grid container">
         <Card />
-      </div>
+        {/* Tudo que está abaixo deve ir para o Card */}
+        {dentistaData.map((item) => (
+        <div className="card-grid container" 
+        key={item.matricula}>
+          <img
+          className="card-img-top"
+          src="/images/doctor.jpg"
+          alt="doctor placeholder"/>
+           <a href={`/dentist/MatriculaDoDentista`}>{item.matricula}
+           <h5 className={`card-title`}>{item.nome}</h5>
+           </a>                 
+        </div>        
+      ))}
+      </div>      
     </>
   );
 };
