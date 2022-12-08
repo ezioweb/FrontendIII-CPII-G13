b75f377bc9b98327e8fda1ca448738dcbd684f05
ezioweb <ezioweb@hotmail.com>
2022-12-07 23:32:08 -0300

    first commit

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Card from "../Components/Card";
+import { ctdUrl } from "../urls";
+
+const Home = () => {
+
+  useEffect(() => {
+    //Nesse useEffect, deverá ser obtido todos os dentistas da API
+    //Armazena-los em um estado para posteriormente fazer um map
+    //Usando o componente <Card />
+    fetch(`${ctdUrl}${'dentista'}`).then(
+      response => console.log(response)
+      )
+      
+    
+
+  }, []);
+
+
+
+  return (
+    <>
+      <h1>Home</h1>
+      <div className="card-grid container">
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default Home;
