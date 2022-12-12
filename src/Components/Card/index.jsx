e68825dc1db051988ef2e2fe0ce722f5e91cfdd5
ezioweb <ezioweb@hotmail.com>
2022-12-12 20:31:48 -0300

    change: CSS to SCSS

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./Card.module.css";
+import styles from "./Card.module.scss";
 import { useTheme } from "../../Hooks/useTheme";
 
 const Card = (props) => {
