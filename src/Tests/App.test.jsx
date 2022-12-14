bcd7bee9b70eabc873d1c928823723e941c6ce2a
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-12-14 16:02:14 -0300

    feat: tests

--- src/Tests/App.test.jsx
+++ src/Tests/App.test.jsx
@@ -1,7 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { ThemeProvider } from "../Hooks/useTheme";
+import { Simulate } from "react-dom/test-utils";
 import App from "../App";
+import Card from "../Components/Card";
 
-const token = localStorage.getItem('token');
+const mockData = {
+    matricula: '123',
+    nome: 'nomedentista',
+    sobrenome: 'sobrenomedentista',
+    usuario: {
+        username: 'usernamedentista'
+    }
+}
+
+const mockTheme = 'dark'
+
+const token = localStorage.getItem('token')
 
 describe('Navbar e footer componentes', () => {
     it('Deveriam ser renderizado em tela', () => {
@@ -9,12 +24,9 @@ describe('Navbar e footer componentes', () => {
 
         expect(screen.getByRole('banner')).toHaveTextContent('DH Odonto')
         expect(screen.getByRole('contentinfo')).toHaveTextContent('Voltar para o topo')
-
     })
-});
 
-describe('Testando dark mode', () => {
-    it('Navbar', () => {
+    it('Testando dark mode - Navbar', () => {
         render(<App />)
 
         const element = screen.getByRole('banner')
@@ -25,7 +37,7 @@ describe('Testando dark mode', () => {
         }
     })
 
-    it('Footer', () => {
+    it('Testando dark mode - Footer', () => {
         render(<App />)
 
         const element = screen.getByRole('contentinfo')
@@ -36,3 +48,53 @@ describe('Testando dark mode', () => {
         }
     })
 });
+
+describe('LoginForm componente', () => {
+    it('Deveria ser renderizado em tela', () => {
+        render(<App />)
+
+        if (token === 'null' || token == null || token.trim() === '') {
+            expect(screen.getByPlaceholderText('Login')).toBeInTheDocument()
+            expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
+        }
+    })
+});
+
+describe('Cards de dentistas componente', () => {
+    it('Deveriam ser renderizado em tela', async () => {
+        render(
+            <MemoryRouter initialEntries={['/home']}>
+                <ThemeProvider value={mockTheme}>
+                    <Card dentista={mockData} />
+                </ThemeProvider>
+            </MemoryRouter>
+        )
+
+        await screen.findAllByText(/nomedentista/i)
+    })
+
+    it('Testando link para o CardDetails', () => {
+        render(
+            <MemoryRouter initialEntries={['/home']}>
+                <ThemeProvider value={mockTheme}>
+                    <Card dentista={mockData} />
+                </ThemeProvider>
+            </MemoryRouter>
+        )
+
+        screen.getByText('nomedentista sobrenomedentista')
+
+        const link = screen.getByText('nomedentista sobrenomedentista')
+
+        Simulate.click(link)
+
+        setTimeout(() => {
+            expect(screen.getByText("Marcar consulta")).toBeInTheDocument()
+        }, 3000)
+    })
+});
+
+describe('Card componente', () => {
+
+});
+
